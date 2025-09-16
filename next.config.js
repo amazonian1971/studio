54b460f9ff572a75d3b84ca830217ee81f5af83c
Tanveer Syed <amazonian1971@gmail.com>
2025-09-16 12:14:08 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Invalid src prop

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatar.vercel.sh',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
