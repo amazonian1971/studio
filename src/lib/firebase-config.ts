fde6e4b4f1b8d0988c5c38f36c94130929c490c1
Tanveer Syed <amazonian1971@gmail.com>
2025-09-16 10:36:11 +0000

    new commit to main

--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -8,3 +8,16 @@ export const firebaseConfig = {
   messagingSenderId: "951302775752",
   appId: "1:951302775752:web:3428b6b936a6dc555556fd"
 };
+{
+  "type": "service_account",
+  "project_id": "trustnet-3fcfa",
+  "private_key_id": "afd54e93f942c9a1592b9d594ea778a99ac4108b",
+  "private_key": "-----BEGIN PRIVATE KEY---\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCXgQ3HJudtDWw/\nUv/9wB+GaAAUM7is4I10Qw0/ETRtQX69dVnLk/GvYTeUGqEbON5zTMd5xzxv2DJI\nRkAsN9RBNfIXeG2QRxhVu5xHcAfWmTrXwJpOIzN0Z1qonrPUNmUrCe5sjDrsOItI\n59LfbkXQGFyVvp/Mf2HfVjtA2Y/wx0heXPI9vNCiYVprxrXTsTExAHeCAFghSH9l\njkeqtQrdjMzRPDECfmsTr9JRhD8HvMFS/IfCgQypW9RGxnlVpe2om9ngfWNPA0tR\nnKQ9WmJ/bWZsEKuQKtl+iDlZ4xvRTYbMaDG4CYYEyBvhG5Xkfjh8wkhviM7QLNmG\npzmyCdtXAgMBAAECggEADknXZ6UQJh/3dGDFZYRKlNhXIDtL21C8QS7tCg8l1M4H\nVXftVZP+Mw5QpGZUiRGLzUT8T8xjXfWpBwRKcMEn1/9DS0UKKZVtl/Gp3gTqAFUS\n5ckzjv375X+1NE+aDIAVeix6xir9iEzBj3PUVp/QSWgax0FN/IaLpPloJ4gSmWBq\nMeOEfL5HPh7aw52AEItSYNmHQPoX0ucYoaEoIDn58FoD0FWad4WzAqKKJg0XlBbu\n9ZDFY6PZA7HIJ49rD6u3W5h02Pi5bGdqElsyBYe3ZgBY/VQkUXsapb3Hi3MR8Xct\n68mzI0EtvVMPAbW77pvEkwf/Hpdy/4SfEVU09BFkvQKBgQDRkObtcfccgaagUx4O\nRjc+ivwdzwXUnXY3NQIQk7Yqrl/IQdntDVOshVYxd7eJS6kw7fNyg51OUJt5a1LI\nMlF02Z7r2WS4tt7p5/TlSAvQYNrvTopMDqzxv1+Ky0gmBiKNn7Adtdo942p1UQN7\nHrvfDWJROEH+GcjJzg4sRflhlQKBgQC5Erz8wcXF8uch1/rgVdswBDoX5FzNBNYF\npEXCRjAGi3jAV+hKKkzC5CHjXy82EaAkkaPyuGFtlgZEucTVF6U5BKyEtfDlMit/\nowzP0el28KEEbzQP0ZdbwlEPUI5MJkFtQgYXvPKcaqStIyPcjETkeJSi/rQEmqgE\nu7o1tDNmOwKBgQDLuH4U403io1K01jgIjk+T+A5AeAjl+DB8WIZqUP2snhi0lvdf\nzy3BL1lr+0IrMKcHzrlzX+iiwg9xyFgQD4JKAmfQRGP6gKNQZVIXNaNubUlHi2U7\nrKSI2Y3qYdUAScR7Qno1V5wRv6q04w1sOaNOoTcrV3Inry8Wpr32YgucxQKBgG2J\nxqt+8NSRSPXCBG2kwz9vMQcVFUwPEbFzIgqzSheFkymcnhLPaHFtXxVZL/M7iWI7\nhSLSuG+I/RrOLM7G2PafapB9sx+kixRXZ+zU2+6Wy2Z5fwHh1kjsANgkoKQ3fD/z\ndPG11o53nBIRm1cc+mWTbL3wLJIRC2MJzVOd3VIjAoGAR2t21pn8AqXsN7HmVvtu\ns1Q3sy8boaRvff3qr2DI1oh/FaaPniqIL6K3y2uslrron8Y48EvyR9PwL3ca6k92\nkBx4QsEJmfkJdutFFxXVryv1DciYS3DZhT6Gm91z4fG6sijVpBrvrNuiW1kKVO1k\nk8+kBIaSMN+5aENjDsp+tmg=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-fbsvc@trustnet-3fcfa.iam.gserviceaccount.com",
+  "client_id": "113961940232164864108",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40trustnet-3fcfa.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
